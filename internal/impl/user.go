37f350097882bf16b63e8780e1a7156e56aab296
Ignacio Sanchez Gines <nacho@geardome.com>
2020-11-30 16:04:10 +0100

    Fix deadlines and keepalive params

--- internal/impl/user.go
+++ internal/impl/user.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/Pallinder/go-randomdata"
 	"github.com/drhelius/grpc-demo-proto/user"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Server struct {
@@ -19,6 +21,11 @@ func (s *Server) Create(ctx context.Context, in *user.CreateUserReq) (*user.Crea
 
 	r := &user.CreateUserResp{Id: strconv.Itoa(randomdata.Number(1000000))}
 
+	err := failedContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
 	log.Printf("[User] Create Res: %v", r.GetId())
 
 	return r, nil
@@ -30,7 +37,26 @@ func (s *Server) Read(ctx context.Context, in *user.ReadUserReq) (*user.ReadUser
 
 	r := &user.ReadUserResp{User: &user.User{Id: in.GetId(), Name: randomdata.FullName(randomdata.RandomGender), Email: randomdata.Email()}}
 
+	err := failedContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
 	log.Printf("[User] Read Res: %v", r.GetUser())
 
 	return r, nil
+}
+
+func failedContext(ctx context.Context) error {
+	if ctx.Err() == context.Canceled {
+		log.Printf("[Order] context canceled, stoping server side operation")
+		return status.Error(codes.Canceled, "context canceled, stoping server side operation")
+	}
+
+	if ctx.Err() == context.DeadlineExceeded {
+		log.Printf("[Order] dealine has exceeded, stoping server side operation")
+		return status.Error(codes.DeadlineExceeded, "dealine has exceeded, stoping server side operation")
+	}
+
+	return nil
 }
